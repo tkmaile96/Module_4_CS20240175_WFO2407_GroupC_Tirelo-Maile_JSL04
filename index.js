64780b7b89c750f6ccd0bf00069929e25a355ffc
Teekay <tkmaile96@gmail.com>
2024-09-06 14:48:04 +0200

    flavour selection preference

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(userPreference === 'sugar-free'? 'sugar-free' : 'Regular');
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
